5a825bbbe4f415f0d3a90db83d518aaa0d4098f6
amosehiguese <amosehiguese@gmail.com>
2023-03-20 21:24:42 +0100

    Add list and detail templates

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,4 @@
+from django.urls import reverse
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
@@ -30,3 +31,6 @@ class Post(models.Model):
       
     def __str__(self) -> str:
         return self.title
+    
+    def get_absolute_url(self):
+        return reverse('blog:post_detail', args=[self.publish.year, self.publish.month, self.publish.day, self.slug])
