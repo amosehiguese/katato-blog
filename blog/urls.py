4a8aa9a2ca48bc360dc9c7fba9ff27f2f0587efa
amosehiguese <amosehiguese@gmail.com>
2023-03-22 00:28:36 +0100

    Add url pattern for sharing post

--- blog/urls.py
+++ blog/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     # post views
     path('', views.PostListView.as_view(), name='post_list'),
     path('<int:year>/<int:month>/<int:day>/<slug:post>', views.post_detail, name='post_detail'),
+    path('<int:post_id>/share/', views.share_post, name='share_post')
 ]
