54292514c9c0b3d2bbcf19415d54d903375cf5ba
amosehiguese <amosehiguese@gmail.com>
2023-03-23 23:41:26 +0100

    Modify comment model

--- blog/models.py
+++ blog/models.py
@@ -43,3 +43,9 @@ class Comment(models.Model):
     created = models.DateField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     active = models.BooleanField(default=True)
+
+    class Meta:
+        ordering = ('created',)
+    
+    def __str__(self) -> str:
+        return f'Comment by {self.name} on {self.post}'
