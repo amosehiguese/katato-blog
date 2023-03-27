95a8edba8f8e51d2ef3260eeab7e2dc2bbd174ee
amosehiguese <amosehiguese@gmail.com>
2023-03-27 19:53:56 +0100

    Implement Django built-in synd feed framework to generate RSS feeds

--- blog/urls.py
+++ blog/urls.py
@@ -3,6 +3,7 @@ from django.contrib import admin
 from django.contrib.sitemaps.views import sitemap
 
 from blog.sitemaps import PostSitemap
+from .feeds import LatestPostsFeed
 
 from . import views
 
@@ -18,5 +19,6 @@ urlpatterns = [
     path('tag/<slug:tag_slug>', views.PostListView.as_view(), name='post_list_by_tag'),
     path('<int:year>/<int:month>/<int:day>/<slug:post>', views.post_detail, name='post_detail'),
     path('<int:post_id>/share/', views.share_post, name='share_post'),
-    path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap')
+    path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap'),
+    path('feed/', LatestPostsFeed(), name='post_feed')
 ]
